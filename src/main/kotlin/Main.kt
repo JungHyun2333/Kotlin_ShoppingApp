42d14c16505d0bbd8f6da5c94d3b98301ad33e16
junghyun <wjdgus2333@gmail.com>
2023-01-06 16:03:26 +0900

    사용자 이름과 나이 입력 받고 인사말 출력

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("안녕하세요. 쇼핑몰입니다. 이름을 입력해주세요.")
+    val name = readLine()
+    println("나이를 입력해주세요")
+    var age: Int = Integer.valueOf(readLine())
+    println("안녕하세요 ${age}살 ${name}님의 개인 스타일리스트가 되어 드릴게요!")
 }
